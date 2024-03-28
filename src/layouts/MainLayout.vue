e229ffe5af38cffdcd223a881b95f2118153304e
root <root@DESKTOP-LCJMKMK>
2024-03-28 05:40:56 +0000

    my landing page

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lff">
+    <q-header dark class="bg-green">
+      <q-toolbar>
+        <q-avatar>
+        </q-avatar>
+        <q-toolbar-title>
+          Home
+        </q-toolbar-title>
+        <q-space />
+        <q-btn flat to="/layout1">Contact</q-btn>
+        <q-btn flat to="/layout2">a propos</q-btn>
+        <q-btn flat to="/layout1">Nos services</q-btn>
+
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <Footer />
+  </q-layout>
+</template>
+
+<script>
+import Footer from "../components/Footer";
+
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  components: {
+    Footer
+  }
+};
+</script>
